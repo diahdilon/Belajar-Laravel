5736989ec54884f51ea70b78b3d253513b0031c5
diah <www.diahapril@gmail.com>
2020-04-23 09:15:07 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,14 @@
 |
 */
 
-//Route::get('/', function () {
-    //return view('index');
-//});
-//Route::get('/about', function () {
-       // return view('about');
-//});
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/about', function () {
+        return view('about');
+});
 
- routes::get('/', 'PagesController@home');
- routes::get('/about', 'PagesController@about');
+ //routes::get('/', 'PagesController@home');
+ //routes::get('/about', 'PagesController@about');
 
 
