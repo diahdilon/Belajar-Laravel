ba530c320213d7b3b08a6d5038ef33db64a89ec8
diah <www.diahapril@gmail.com>
2020-04-27 19:46:12 +0700

    DataController.php

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Appp\Student;
 
 class DataController extends Controller
 {
@@ -14,7 +15,8 @@ class DataController extends Controller
      */
     public function index()
     {
-        $data = DB::table('data')->get();
+        //$data = DB::table('data')->get();
+        $data = Student::all();
        return view('data.index',['data'=> $data]);
     }
 
