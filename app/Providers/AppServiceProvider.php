a753888f9766b0407c4f47642eb2cd6a28f163ef
diah <www.diahapril@gmail.com>
2020-04-28 20:20:23 +0700

    AppServiceProvide.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -16,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(255);
     }
 
     /**
